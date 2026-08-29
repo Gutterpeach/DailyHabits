3526a504add7899e4f0816ce9f9d1fcc4b0bf8ff
Gutterpeach <sarahkubar@gmail.com>
2026-08-29 18:12:33 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,21 @@
-// Minimal service worker for Samsung Internet PWA support
-self.addEventListener('fetch', (e) => {});
+const CACHE_NAME = 'habit-tracker-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
